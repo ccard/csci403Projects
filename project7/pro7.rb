c35f39f1340eb76667b6fc537c875843b7ba6122
Chris Card <ccard@mymail.mines.edu>
2013-03-20 15:01:43 -0600

    added some comments to the rb file

--- project7/pro7.rb
+++ project7/pro7.rb
@@ -7,6 +7,8 @@ begin
 	con = Mysql.new 'csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com', 'ccard', 'gArfiEld!'
 	puts con.get_server_info
 	con.query 'USE snacks'
+	rs = con.query 'SELECT name FROM project_test' # rs.each do |row|
+							#pust row['key']
 	#puts rs.fetch_row
 rescue Exception => e
 	puts e.errno
