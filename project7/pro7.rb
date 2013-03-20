b5c693202a19f5779d71084e4be8eb951b21a168
Chris Card <ccard@mymail.mines.edu>
2013-03-20 15:21:34 -0600

    commented and is working as expected

--- project7/pro7.rb
+++ project7/pro7.rb
@@ -4,17 +4,23 @@
 require 'mysql'
 
 begin
+	#Creates a new connection to sever <location>,<user>,<pass>,<database>
 	con = Mysql.new 'csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com', 'ccard', 'gArfiEld!','snacks'
-	rs = con.query 'SELECT name FROM project_test' # rs.each do |row|
-							#pust row['key']
+
+	#queries the db for names in project_test and gets the result set
+	rs = con.query 'SELECT name FROM project_test' 
+
+	#this iterates through each row storing each row into row
 	rs.each() do |row|
-		printf "%s\n", row[0]
+		printf "%s\n", row[0] #prints out the name
 	end
 
+#error catching for exception
 rescue Exception => e
 	puts e.errno
 	puts e.error
 
-ensure
- con.close if con	
+ensure #like finally always ensures that connection is closed
+	#closes connection if conection exists
+	con.close if con	
 end
