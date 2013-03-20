43075814b1d3194158a8f3c72613d02156cd52e3
Chris Card <ccard@mymail.mines.edu>
2013-03-20 14:52:43 -0600

    got i logging into amazone server

--- project7/pro7.rb
+++ project7/pro7.rb
@@ -4,10 +4,10 @@
 require 'mysql'
 
 begin
-	con = Myswl.new ' csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com', 'ccard', 'password here'
+	con = Mysql.new 'csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com', 'ccard', 'gArfiEld!'
 	puts con.get_server_info
-	rs = con.query 'statements'
-	puts rs.fetch_row
+	con.query 'USE snacks'
+	#puts rs.fetch_row
 rescue Exception => e
 	puts e.errno
 	puts e.error
