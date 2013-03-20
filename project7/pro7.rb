c27f7598e41dd7103adb5aa85df69e5cdf843f0d
Chris Card <ccard@mymail.mines.edu>
2013-03-20 15:16:05 -0600

    it is now working ya

--- project7/pro7.rb
+++ project7/pro7.rb
@@ -4,12 +4,13 @@
 require 'mysql'
 
 begin
-	con = Mysql.new 'csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com', 'ccard', 'gArfiEld!'
-	puts con.get_server_info
-	con.query 'USE snacks'
+	con = Mysql.new 'csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com', 'ccard', 'gArfiEld!','snacks'
 	rs = con.query 'SELECT name FROM project_test' # rs.each do |row|
 							#pust row['key']
-	#puts rs.fetch_row
+	rs.each() do |row|
+		printf "%s\n", row[0]
+	end
+
 rescue Exception => e
 	puts e.errno
 	puts e.error
