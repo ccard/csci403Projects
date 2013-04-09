4600fd140da191f25c6ccf9c749692bbf63dc22f
ccard <ccard@mymail.mines.edu>
2013-04-09 09:55:12 -0600

    now have twelve commands for redis working

--- project9/redis.rb
+++ project9/redis.rb
@@ -1,18 +1,24 @@
-#! /user/bin/env ruby
+#! /usr/bin/env ruby
 
 #Chris Card
 #Project 9
 
 require "redis"
+require "json"
 
 @redis = Redis.new :host => "pub-redis-13059.us-west-1.1.azure.garantiadata.com", :port => 13059, :password => "wuwnosql403"
 
 
 def find
-	puts "Find by key:"
-	puts "Enter key:"
-	key = gets.chomp
-	puts "#{@redis.get key}"
+	begin
+		puts "Find by key:"
+		puts "Enter key:"
+		key = gets.chomp
+		puts "#{@redis.get key}"
+
+	rescue Redis::CommandError => e
+		puts "Value for key can not be obtained by gets"
+	end
 
 end
 
@@ -57,6 +63,126 @@ def addm
 	end
 end
 
+def append
+	puts "Append a value to a key"
+	puts "Enter key:"
+	key = gets.chomp
+	puts "Enter value to append:"
+	value = gets.chomp
+
+	puts "#{@redis.append key,value}"
+end
+
+def storeList
+	puts "Store a list of values"
+	puts "Enter key for list (existing|new):"
+	key = gets.chomp
+
+	while true
+		puts "Enter value or '!end' to stop input:"
+		value = gets.chomp
+		value == "!end" ? break : ""
+		@redis.lpush key, value
+	end
+end
+
+def getList
+	puts "Get a list"
+	puts "Enter key of list:"
+	key = gets.chomp
+	begin
+		length = @redis.llen key
+		list = @redis.lrange key, 0, length
+		list.empty? ? "#{puts "there is no such list"}" : 
+				list.each do |element| 
+					puts "#{element}" 
+				end
+	rescue Redis::CommandError => e
+		puts "The key entered doesn't map to a list"
+	end
+end
+
+def getElement
+	puts "Get an element of a list"
+	puts "Enter key of list:"
+	key = gets.chomp
+	begin
+		length = @redis.llen key
+		puts "Enter the index you want between 0 and #{length-1}"
+		index = gets.chomp
+		puts "Element at #{index} is: #{@redis.lindex key,index}"
+	rescue Redis::CommandError => e
+		puts "Either you put in the incorrect index or the key doesn't map to a list"
+	end
+end
+
+def createSet
+	puts "Create or add to a set"
+	puts "Enter key for set:"
+	key = gets.chomp
+	begin
+		@redis.smembers key
+
+		while true
+			puts "Enter value or '!end' to terminate input"
+			value = gets.chomp
+			value == "!end" ? break : ""
+			@redis.sadd key,value
+		end
+
+	rescue Redis::CommandError => e
+		puts "Invalid set or key already exists and is not a set"
+	end
+end
+
+def findIntersecion
+	puts "Find the intersection of 2 sets"
+	puts "Enter key of set 1:"
+	key1 = gets.chomp
+	puts "Enter key of set 2:"
+	key2 = gets.chomp
+
+	begin
+		puts "#{@redis.sinter key1,key2}"
+	rescue Redis::CommandError => e
+		puts "One of the sets may not exist"
+	end
+end
+
+def getSet
+	puts "Get a set"
+	puts "Enter set key:"
+	key = gets.chomp
+
+	begin
+		members = @redis.smembers key
+		members.each do |member|
+			puts "#{member}"
+		end
+	rescue Redis::CommandError => e
+		puts "Set doesn't exist or key doesn't map to a set"
+	end
+end
+
+def getKey
+	puts "Get a key matching a pattern"
+	puts "Uses: ? mathes any character only once"
+	puts "      * 0->any number of characters"
+	puts "      [] mathes characters provided and nothing else"
+	puts "Enter pattern:"
+	pattern = gets.chomp
+
+	begin
+		puts "The matching keys are:"
+		keys = @redis.keys pattern
+		keys.empty? ? "#{puts "no keys matching pattern found"}":
+				keys.each do |key|
+					puts "#{key}"
+				end
+	rescue Redis::CommandError => e
+		puts "Invalid pattern"
+	end
+end
 
 def main_menu
   puts "\nMain Menu."
@@ -64,8 +190,14 @@ def main_menu
   puts "B. Add obj"
   puts "C. Increment count"
   puts "D. Add multiple objects"
-  puts "E. Find a Snack"
-  puts "F. Add a New Snack"
+  puts "E. Append to value to key"
+  puts "F. Store a list of values"
+  puts "G. Get a list"
+  puts "H. Get element of list"
+  puts "I. Create or add to a set"
+  puts "J. Find intersection of two sets"
+  puts "K. Get a set"
+  puts "L. Get a keys matching a pattern"
   puts "Q. Quit"
 end
 
@@ -79,12 +211,22 @@ def execute_command(command)
     incer
   when "D"
   	addm
-  #when "E"
-  #  puts "\nFind a Snack"
-  #  find_snack
-  #when "F"
-  #  puts "\nAdding a new Snack"
-  #  add_snack
+  when "E"
+    append
+  when "F"
+    storeList
+  when "G"
+  	getList
+  when "H"
+  	getElement
+  when "I"
+  	createSet
+  when "J"
+  	findIntersecion
+  when "K"
+  	getSet
+  when "L"
+  	getKey
   when "Q"
     puts "Quitting... buh-bye."
   else
