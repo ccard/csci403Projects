2bf723cece02a4b18cea79edf6bdb8210d4fc23e
ccard <ccard@mymail.mines.edu>
2013-04-18 10:58:35 -0600

    nothing chagned

--- project10/zoolicious.rb
+++ project10/zoolicious.rb
@@ -2,13 +2,20 @@
 #Chris Card
 
 require 'mongo'
-require 'uri'
+require 'json'
 
-MONGO_URL='mongodb://junk@humanoriented.com:wuwnosql403@staff.mongohq.com:10033/zoolicious'
-db = URI.parse(ENV[MONGO_URL])
-db_name = db.path.gsub('/^\//','')
-@connect = Mongo::Connection.new(db.host,db.port).db(db_name)
-@connect.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+#MONGO_URL="mongodb://junk:wuwnosql403@staff.mongohq.com:10033/zoolicious"
+#db = URI.parse(MONGO_URL)
+#db_name = db.path.gsub('/^\//','')
+#@connect = Mongo::Connection.new(db.host,db.port).db(db_name)
+#@connect.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+
+@db = Mongo::MongoClient.new('staff.mongohq.com',10033).db("zoolicious")
 
 
-@connect.close
+
+def listAll
+
+	zoos = db.zoo.find
+	zoos.forEach(printjson)
+end
