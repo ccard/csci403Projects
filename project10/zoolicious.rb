fa0f867a4e014a00f574599bb2ff653251ab232d
ccard <ccard@mymail.mines.edu>
2013-04-18 11:18:51 -0600

    got it authenticating corectly

--- project10/zoolicious.rb
+++ project10/zoolicious.rb
@@ -1,25 +1,18 @@
-#! /usr/bin/env ruby
+#! usr/bin/env ruby
 #Chris Card
 
 require 'mongo'
-<<<<<<< HEAD
-=======
-require 'uri'
->>>>>>> 996f5f705011d4ee3f386a1a72f487758887c1bd
+require "uri"
 require 'json'
 
-#MONGO_URL="mongodb://junk:wuwnosql403@staff.mongohq.com:10033/zoolicious"
-#db = URI.parse(MONGO_URL)
-#db_name = db.path.gsub('/^\//','')
-#@connect = Mongo::Connection.new(db.host,db.port).db(db_name)
-#@connect.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
 
-@db = Mongo::MongoClient.new('staff.mongohq.com',10033).db("zoolicious")
+@db = Mongo::MongoClient.new("staff.mongohq.com",10033).db("zoolicious")
+@db.authenticate "ccard","wuwnosql403"
 
 
 
 def listAll
 
-	zoos = db.zoo.find
+	zoos = connect.zoo.find
 	zoos.forEach(printjson)
 end
